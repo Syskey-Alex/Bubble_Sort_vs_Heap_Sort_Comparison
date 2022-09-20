4788757c726282047ed442331334b618cfa44e75
Alexandru <79929966+Syskey-Alex@users.noreply.github.com>
2022-09-20 08:26:46 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
     //56,78,3,4,16,112,512,5,2
 static int[] array = new int[]{56,78,3,4,16,112,512,5,2};
     public static void main(String[] args) {
-        //     bubbleSort(array);
+        bubbleSort(array);
         int arr[] = {12, 11, 13, 5, 6, 7};
         int N = arr.length;
 
@@ -35,7 +35,7 @@ static int[] array = new int[]{56,78,3,4,16,112,512,5,2};
             countSort++;
         }
         for(int i=0;i<n;i++){
-            System.out.print(array[i]+" ");
+            System.out.print("This is bubble sort: "+ array[i]+" ");
         }
     }
 
